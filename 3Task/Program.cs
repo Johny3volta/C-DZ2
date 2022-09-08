3212ea477c6a96036ac03e5bdfb006430f082069
3v <jarovcev@mail.ru>
2022-09-08 11:27:05 +0300

    all Task

---
+++ 3Task/Program.cs
@@ -0,0 +1,11 @@
+﻿//Проверяет дни недели
+Console.Write("Введите Число Дня недели  ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <= 5)
+    Console.WriteLine( "NOOOO");
+else
+    Console.WriteLine( "Yessss");
+
+
+
+
