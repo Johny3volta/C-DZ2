c187f261be7e9dbd6eeb537bb2a23fa8dd748f1c
3v <jarovcev@mail.ru>
2022-09-08 10:26:08 +0300

    2task

---
+++ 2Task/Program.cs
@@ -0,0 +1,9 @@
+﻿//Показывает 3ю цифру 3х значного чила или сообщает об ошибке.
+Console.Write("Введите Трехзначное число  ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 99)
+    Console.WriteLine( "ERROR!");
+else
+    Console.WriteLine(Convert.ToString(n)[2]);
+
+
